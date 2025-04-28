415e6a2241ed05d747aacb0b2abf69bb312e75bc
Felgi <felipeferraridoval@gmail.com>
2025-04-28 12:03:57 -0300

    funciona con descuento y corregidos errores y coments

--- Assets/Scripts/FlotaTaxis.cs
+++ Assets/Scripts/FlotaTaxis.cs
@@ -14,31 +14,33 @@ public class FlotaTaxis : MonoBehaviour
     private int cantLitrosDia;
     private float precioTotal;
     private int cantLitrosTotales;
+    private float descuento = .8f;
 
     void Start()
     {
-        cantLitroPorUnidad = kmPorUnidad / kmPorLitro;
-        if (flotaCant < 0 || dias < 0)
+        cantLitroPorUnidad = kmPorUnidad / kmPorLitro; //Consigo la relacion de cantidad de litros por lo que hace una unidad por dia, que es 6 litros
+        if (flotaCant < 0 || dias < 0) // Si la flota es menor a 0 o si los dias son menores a 0
         {
-            Debug.Log("Ambos valores tienen que ser igual o mayor que 0");
+            Debug.Log("Ambos valores tienen que ser igual o mayor que 0"); 
             return;
         }
         if (dias < 5)
         {
-            Debug.Log("La cantidad de dias no puede ser menor a 5");
+            Debug.Log("La cantidad de dias no puede ser menor a 5"); // Si los dias son menores a 5
+            return;
+        }
+        cantLitrosDia = flotaCant * cantLitroPorUnidad; // La cantidad de litros que consume la flota por dia 
+        cantLitrosTotales = cantLitrosDia * dias; // Multiplico lo que consume de litros la flota en un dia por la cantidad de dias que puso el usuario
+
+        precioTotal = cantLitrosTotales * precioPorLitro; //El precio total son los litros totales que consumieronla flota y lo multiplico por la variable precio por litro
+        if (cantLitrosTotales >= 100) // Si la cantidad de litros totales son iguales o mayores a 100 litros entonces se aplica un descuento del 20%
+        {
+            precioTotal = precioTotal - (precioTotal * descuento); // precio total con el descuento aplicado
+            Debug.Log("Una flota de " + flotaCant + " unidades trabajando durante " + dias + " días implicará un gasto de " + precioTotal + " pesos sumado a un descuento de " + descuento + " en concepto del combustible total: " + cantLitrosTotales);
             return;
         }
 
-
-        cantLitrosDia = flotaCant * cantLitroPorUnidad;
-        cantLitrosTotales = cantLitrosDia * dias;
-        precioTotal = cantLitrosTotales * precioPorLitro;
-        if (cantLitroPorUnidad % 2 != 0)
-        {
-            precioTotal = precioTotal - precioTotal * .8;
-        }
-
-        Debug.Log("Una flota de " + flotaCant + " unidades trabajando durante " + dias + " días implicará un gasto de " + precioTotal + " pesos en concepto de combustible");
+        Debug.Log("Una flota de " + flotaCant + " unidades trabajando durante " + dias + " días implicará un gasto de " + precioTotal + " pesos en concepto del combustible");
 
     }
 
